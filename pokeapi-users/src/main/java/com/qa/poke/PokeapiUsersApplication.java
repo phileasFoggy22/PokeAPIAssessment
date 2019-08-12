dda4baa57c361b3263cfa69ac3b23ec8e614fdbe
phileasfoggy22 <phileas.foggy@googlemail.com>
2019-08-12 11:35:29 +0100

    test page tomcat

---
+++ pokeapi-users/src/main/java/com/qa/poke/PokeapiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.qa.poke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeapiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeapiUsersApplication.class, args);
+	}
+
+}
